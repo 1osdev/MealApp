08a7977634fcccc89c2e12ab4b29eb6a6f129a6b
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-02-18 02:46:17 +0300

    project changed #2

--- MealApp/MealApp/Models/Meal.swift
+++ MealApp/MealApp/Models/Meal.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Meal {
+    let name: String
+    let price: String
+    let image: String
+    }
+
+extension Meal {
+    static func getMeals() -> [Meal] {
+        [
+            Meal(name: "Пицца",
+                price: "15 BYN",
+                image: "pizza"),
+            Meal(name: "Кока -Кола",
+                price: "3 BYN",
+                image: "cola"),
+            Meal(name: "Мороженое",
+                price: "5 BYN",
+                image: "icecream")
+        ]
+    }
+}
+
