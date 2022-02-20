63e9c1a204cac0baff5f6d6a073124f876ff167d
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-02-20 19:21:00 +0300

    project changed #3

---
+++ MealApp/MealApp/Controllers/ReviewTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ReviewTableViewCell.swift
+//  MealApp
+//
+//  Created by Дмитрий Смирнов on 19.02.22.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var rewievLabel: UILabel!
+    @IBOutlet var markLabel: UILabel!
+}
--- MealApp/MealApp/Models/Meal.swift
+++ MealApp/MealApp/Models/Meal.swift
@@ -5,27 +5,65 @@
 //  Created by Дмитрий Смирнов on 17.02.22.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - Meal
 
 struct Meal {
-    let name: String
-    let price: String
-    let image: String
+    // MARK: Internal
+
+    var name: String
+    var price: Double
+    var image: UIImage?
+
+    var feadbacks: [Feadback] = []
+
+    // Вычисляемые свойства которые помогают отображать данные
+    
+    var ratingBar: String {
+        if let rating = rating {
+            return String(repeating: "⭐️", count: Int(rating.rounded(.up))) // control + command + " "
+        } else {
+            return "There are no reviews yet"
+        }
     }
 
-extension Meal {
-    static func getMeals() -> [Meal] {
-        [
-            Meal(name: "Пицца",
-                price: "15 BYN",
-                image: "pizza"),
-            Meal(name: "Кока -Кола",
-                price: "3 BYN",
-                image: "cola"),
-            Meal(name: "Мороженое",
-                price: "5 BYN",
-                image: "icecream")
-        ]
+    // MARK: Private
+
+    private var rating: Double? {
+        if feadbacks.isEmpty {
+            return nil
+        } else {
+            var sum: Double = 0
+            for rewiev in feadbacks {
+                sum += rewiev.mark
+            }
+            return sum / Double(feadbacks.count)
+        }
+    }
+}
+
+// MARK: - Feedback
+
+struct Feadback {
+    
+    private let date = Date()
+    
+    let text: String
+    let mark: Double
+
+    // Вычисляемые свойства которые помогают отображать данные
+    
+    var ratingBar: String {
+        String(repeating: "⭐️", count: Int(mark.rounded(.up))) // control + command + " "
+    }
+
+    var dateString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        return dateFormatter.string(from: date)
     }
 }
 
